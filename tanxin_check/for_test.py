f657464bd3c3ea02cd952b210939c2b448bfdb63
geyingqi <geyingqi@kuailexue.com>
2018-01-15 14:29:56 +0800

    fix re-assign bug

--- tanxin_check/for_test.py
+++ tanxin_check/for_test.py
@@ -14,30 +14,44 @@ class Solution(object):
         current_length = 0
         self.calculate(origin_nums, current_depth, last_data, current_length)
 
-    def dist(self, x1, x2, last_data):
+    def dist(self, current_data, last_data):
         if not last_data:
             return 0
-        return ((x1 - x2) ** 2 + (last_data[0] - last_data[1]) ** 2) ** 0.5
+        return ((current_data[0] - last_data[0]) ** 2 + (current_data[1] - last_data[1]) ** 2) ** 0.5
 
     def calculate(self, nums, current_depth, last_data, current_length):
         if current_depth == 0:
+            current_length += self.dist(self.first_data, last_data)
             self.length_data.append(current_length)
             if current_length < self.min_length:
                 self.min_length = current_length
             return
         for location in nums:
-            data = self.dist(location[0], location[1], last_data)
-            current_length += data
+            if last_data is None:
+                current_length = 0
+                self.first_data = location
+            data = self.dist(location, last_data)
+            new_length = data + current_length
             left_nums = list(nums)
             left_nums.remove(location)
-            self.calculate(left_nums, current_depth - 1, location, current_length)
+            self.calculate(left_nums, current_depth - 1, location, new_length)
+
+
+def distance_count(list_points):
+    solution = Solution()
+    current_length = 0
+    for i in range(1, len(list_points)):
+        current_length += solution.dist(list_points[i], list_points[i - 1])
+
+    print current_length
 
 if __name__ == '__main__':
     start_time = time.time()
     solution = Solution()
-    nums = [(1, 2), (2, 4), (5, 6), (7, 8), (9, 9), (2, 2), (2, 3), (6, 7), (8, 0), (8, 1), (8, 2)]
+    nums = [(6, 9), (5, 6), (3, 1), (1, 4), (1, 8), (4, 3), (2, 4), (3, 5)]
     # nums = [(1, 2), (2, 4), (5, 6)]
     solution.check_min_data(nums)
     print solution.min_length
-    print len(solution.length_data)
-    print time.time() - start_time
+    # print solution.length_data
+    # distance_count([(9, 8), (4, 4), (3, 9), (4, 9), (9, 9), (9, 8)])
+
