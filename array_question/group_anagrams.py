ee76fc1d0a097109a148f0473c0cb8ba1495997e
geyingqi <geyingqi@kuailexue.com>
2018-03-05 21:09:58 +0800

    update items

--- array_question/group_anagrams.py
+++ array_question/group_anagrams.py
@@ -3,6 +3,21 @@
 # @Time  :  2018/2/26 下午8:27
 
 
+"""
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+Note: All inputs will be in lower-case.
+"""
+
 class Solution(object):
     def groupAnagrams(self, strs):
         """
