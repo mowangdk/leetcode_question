9e9df284da2146a74bd8f562f950b0098a3af576
geyingqi <geyingqi0701@gmail.com>
2018-07-16 16:35:26 +0800

    kmp finish

--- string_question/kmp_questions/basic_kmp.py
+++ string_question/kmp_questions/basic_kmp.py
@@ -4,18 +4,40 @@
 
 class Solution(object):
 
-    def get_next(self, origin_str, next_list):
-        str_length = len(origin_str)
-        next_list[0] = -1
+    def get_next(self, pattern_str):
+        str_length = len(pattern_str)
+        next_list = [-1] * str_length
         k = -1
         j = 0
+        # 这里 P[k] 表示前缀， P[j] 表示后缀
         while j < str_length - 1:
-            if k == -1 or origin_str[j] == origin_str[k]:
+            if k == -1 or pattern_str[j] == pattern_str[k]:
                 k += 1
                 j += 1
                 next_list[j] = k
             else:
                 k = next_list[k]
+        return next_list
+
+    # 为了防止 p[j] == p[next[j]] 的情况，导致重复匹配。我们对getNext方法优化如下
+
+    def get_better_next(self, pattern_str):
+        p_str_len = len(pattern_str)
+        next_list = [-1] * p_str_len
+        k = -1
+        j = 0
+        while j < p_str_len - 1:
+            if k == -1 or pattern_str[j] == pattern_str[k]:
+                j += 1
+                k += 1
+                if pattern_str[j] != pattern_str[k]:
+                    next_list[j] = k
+                else:
+                    # 因为不能出现 p[j] = p[ next[j]], 所以当出现时需要继续递归， k = next[k] = next[next[k]]
+                    next_list[j] = next_list[k]
+            else:
+                k = next_list[k]
+
 
     def kmp_search(self, origin_str, parten, next_list):
         i = 0
@@ -36,3 +58,5 @@ class Solution(object):
 
 if __name__ == '__main__':
     solution = Solution()
+    # print solution.get_next('DABCDABDE')
+    print solution.get_next('ABCDABD')
